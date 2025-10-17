183ff54ab652c001f9212d93c08f502d7801e9f0
Fayaz Sardar <fyz.srd@gmail.com>
2025-10-17 12:27:37 +0530

    feat:checkOut Adrres Box

--- src/layouts/CheckOutLayout.jsx
+++ src/layouts/CheckOutLayout.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { Outlet, useNavigate } from 'react-router-dom'
 
 const CheckOutLayout = () => {
--- src/pages/checkOut/pages/CheckOutPage.jsx
+++ src/pages/checkOut/pages/CheckOutPage.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import CheckOutAdressBox from '../components/CheckOutAdressBox'
+import { useGetAddressesQuery } from '../../../features/profile/api/profileApi'
+import CheckOutAddressModal from '../components/CheckOutAddressModal'
 
 const CheckOutPage = () => {
+   const { data: addressData = [], isLoading } = useGetAddressesQuery()
+  const [selectedAddress, setSelectedAddress] = useState(null)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  // pick default address once data loads
+  useEffect(() => {
+    const defaultAddr = addressData.find((a) => a.defaultAddress)
+    if (defaultAddr) setSelectedAddress(defaultAddr)
+  }, [addressData])
   return (
-     <div className="max-w-4xl mx-auto bg-white p-6 rounded shadow">
-        <div className='flex'>
-            <h1>order Sumemry</h1>
-            <button>Close</button>
+     <div className="max-w-4xl mx-auto bg-white p-4 rounded shadow">
+        <div>
+          <CheckOutAdressBox
+          address={selectedAddress}
+          onAdd={() => console.log('Open add address form')}
+        onChange={() => setIsModalOpen(true)}
+        isLoading={isLoading}
+           />
         </div>
       <h1 className="text-2xl font-bold mb-4">Checkout</h1>
       {/* Implement checkout form / summary here */}
       <p>Checkout form and order summary goes here...</p>
+
+      {isModalOpen && (
+        <CheckOutAddressModal
+        addresses={addressData}
+         onClose={() => setIsModalOpen(false)}
+        onSelect={(addr)=>{
+          setSelectedAddress(addr),
+          setIsModalOpen(false)
+        }}
+        />
+      )}
     </div>
   )
 }
